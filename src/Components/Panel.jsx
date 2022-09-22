47de3fceca086e772271e4991885e29b9a0925db
ACsóka <acsokacsaba@gmail.com>
2022-09-22 18:47:14 +0200

    Cleaned up

---
+++ src/Components/Panel.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Panel extends Component {
+    state = {  } 
+    render() { 
+        return (
+        <React.Fragment></React.Fragment>);
+    }
+}
+ 
+export default Panel;
