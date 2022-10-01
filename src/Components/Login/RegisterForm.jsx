3602068c16c8fc486ef5bc87ca51b80f69fbbc16
Csóka Csaba <acsokacsaba@gmail.com>
2022-10-02 00:51:47 +0200

    Added Registration Form

--- src/Components/Login/RegisterForm.jsx
+++ src/Components/Login/RegisterForm.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import "./css/RegisterForm.css";
+
 import InputPlus from "./InputPlus"
 
 
@@ -9,7 +11,15 @@ class RegisterForm extends Component {
         return (
             <React.Fragment>               
                 <form>
-                <InputPlus left="15%" width="70%" top="32.5%" height="5%"  type="email" placeholder="Vezetéknév" autocompleteID="email"/>
+                    <InputPlus left="15%" width="32.5%" top="25.5%" height="5%"  type="text" placeholder="Vezetéknév" autocompleteID="family-name"/>
+                    <InputPlus left="52.5%" width="32.5%" top="25.5%" height="5%"  type="text" placeholder="Keresztnév" autocompleteID="given-name"/>
+
+                    <InputPlus left="15%" width="70%" top="40.5%" height="5%"  type="email" placeholder="Email" imageSrc="Email" autocompleteID="email"/>
+
+                    <InputPlus left="15%" width="70%" top="55.5%" height="5%" type="password" placeholder="Jelszó" imageSrc="Key" autocompleteID="new-password"/>  
+                    <InputPlus left="15%" width="70%" top="70.5%" height="5%" type="password" placeholder="Jelszó" imageSrc="Key" autocompleteID="new-password"/>
+
+                    <button type="button" className='registerBtn rounded-pill'>Regisztráció</button> 
                 </form>
 
             </React.Fragment>
