34ef972c56b00690dd65cc963407aa1bb621edc3
ACsóka <acsokacsaba@gmail.com>
2022-10-20 14:03:08 +0200

    Added Sequalize connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const {Sequelize} = require('sequelize');
 const app = express();
 const port = process.env.PORT || 5000;
 
+require('dotenv').config();
+
+
+const sequelize = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, process.env.SQL_PASSWORD, {
+    host: process.env.SQL_HOST,
+    dialect: "mysql",
+    port: parseInt(process.env.SQL_PORT),
+});
+
+sequelize.authenticate().then(() => {
+    console.log('[Adatbázis]: A kapcsolat sikeresen létrejött!');
+ }).catch((error) => {
+    console.error('[Adatbázis]: ', error);
+ });
+
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
