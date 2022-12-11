e93961f8661349165ccedc969341bf098b666c47
Csóka Csaba <acsokacsaba@gmail.com>
2022-12-11 02:58:56 +0100

    Fixed checkNewPasswordKey
    Inverted the function. Now only sends response, when the link is valid.

--- backend/paths/checkNewPasswordKey.js
+++ backend/paths/checkNewPasswordKey.js
@@ -1,9 +1,10 @@
 module.exports = (app, sql_con) => {
     app.post("/checkNewPasswordKey", async (req, res) => {
+
         sql_con.query("SELECT Time FROM ReserveIt_ForgottenPasswordData WHERE VerificationID=? LIMIT 1", [req.body.EditKey], function (err, result){
-            if(result.length == 0){
-                res.send({State: false})
-                return false
+            if(result.length == 1){
+                res.send({State: true})
+                return true
             }
         })
     })
