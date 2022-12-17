b573b00d6b562addb55c3c4bea10dd246588f52e
Csóka Csaba <acsokacsaba@gmail.com>
2022-12-17 15:40:54 +0100

    Finished /changePassword

--- backend/paths/changePassword.js
+++ backend/paths/changePassword.js
@@ -4,8 +4,9 @@ module.exports = (app, isFieldEmpty, sql_con, bcrypt, mail_con) => {
             "Password": "",
             "RePassword": "",
         }
-        sql_con.query("SELECT * FROM ReserveIt_ForgottenPasswordData WHERE VerificationID=? LIMIT 1", [req.body.EditKey], async function (err, result){
+        sql_con.query("SELECT ReserveIt_ForgottenPasswordData.AccountID, ReserveIt_Accounts.Password FROM ReserveIt_ForgottenPasswordData INNER JOIN ReserveIt_Accounts ON ReserveIt_Accounts.AccountID = ReserveIt_ForgottenPasswordData.AccountID WHERE VerificationID=? LIMIT 1", [req.body.EditKey], async function (err, result){
             if(result.length == 0){
+
                 res.send({Errors: tempErr, Code: "REDIRECT-LOGIN"})
                 return false
             }
@@ -31,9 +32,19 @@ module.exports = (app, isFieldEmpty, sql_con, bcrypt, mail_con) => {
                 return false
             }
 
+            if(await bcrypt.compare(req.body.password, result[0].Password)){
+                tempErr["Password"] = "A jelszavad nem lehet ugyanaz mint a jelenlegi!"
+                tempErr["RePassword"] = "A jelszavad nem lehet ugyanaz mint a jelenlegi!"
+                res.send({Errors: tempErr, Code: ""})
+                return false
+            }
+
             const pass = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
             sql_con.query("UPDATE ReserveIt_Accounts SET Password=?", [pass], function(err) {
                 tempErr["Password"] = "A jelszavad sikeresen megváltoztattuk!"
+                tempErr["RePassword"] = "A jelszavad sikeresen megváltoztattuk!"
+                res.send({Errors: tempErr, Code: "REDIRECT-LOGIN"})
+                sql_con.query("DELETE FROM ReserveIt_ForgottenPasswordData WHERE AccountID=?", [result[0].AccountID], function(err) {})
             })
         })
     })
--- src/Components/Login/NewPasswordForm.jsx
+++ src/Components/Login/NewPasswordForm.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom'
+import {useParams, useNavigate} from 'react-router-dom'
 
 import "./css/ForgottenPasswordForm.css";
 
@@ -9,7 +9,8 @@ import InputPlus from "./InputPlus"
 
 export default function ForgottenPasswordForm() {
     const {Key} = useParams()
-    const [Display, setDisplay] = useState(<div className='noLink'>Hiba a szerverre való kapcsolódás során!</div>)
+    const navigate = useNavigate();
+    const [Display, setDisplay] = useState("NoCon")
     const [Password, setPassword] = useState("")
     const [RePassword, setRePassword] = useState("")
     const [Errors, setErrors] = useState({
@@ -17,14 +18,120 @@ export default function ForgottenPasswordForm() {
         "RePassword": ""
     })
 
+    function getPage(){
+        return(
+            <React.Fragment>
+            <form onSubmit={(event) => {
+                    event.preventDefault()
+                    FieldCheck({
+                        password: Password,
+                        RePassword: RePassword
+                    })
+                }}>
+                <div className='forgotExplain'>Add meg az új jelszavadat!</div>
+                <InputPlus left="15%" width="70%" top="45%" height="5%"  type="password" placeholder="Jelszó" imageSrc="Key" autocompleteID="new-password" dataSet={(txt) => {setPassword(txt); clearError("Password")}} ErrorMsg={Errors["Password"]}/>
+                <InputPlus left="15%" width="70%" top="60%" height="5%"  type="password" placeholder="Jelszó megerősítés" imageSrc="Key" autocompleteID="new-password" dataSet={(txt) => {setRePassword(txt); clearError("RePassword")}} ErrorMsg={Errors["RePassword"]}/>
+                <button type="submit" className='loginBtn rounded-pill' style={{
+                    top: "80%"
+                }}>Megváltoztatás</button>
+            </form>
+            </React.Fragment>
+        )
+    }
+
+    
+
     function clearError(ID) {
         var tempErr = {...Errors}
         tempErr[ID] = ""
+        if(ID === "Password" || ID === "RePassword"){
+            tempErr["Password"] = ""
+            tempErr["RePassword"] = ""
+        }
         setErrors(tempErr)
     }
 
+    function FieldCheck(dict){
+        var tempErr = {
+            "Password": "",
+            "RePassword": ""
+        }
+        if(isFieldEmpty(dict.password)){
+            tempErr["Password"] = "Üres mező!"
+            setErrors(tempErr)
+        }
+
+        if(isFieldEmpty(dict.RePassword)){
+            tempErr["RePassword"] = "Üres mező!"
+            setErrors(tempErr)
+        }
+
+
+        if(isThereError(tempErr)){ //Ha volt hiba, akkor beállítja a hibákat és nem engedi tovább
+            setErrors(tempErr)
+            return false
+        }
+
+        if(dict.password !== dict.RePassword){
+            tempErr["Password"] = "A két jelszó nem egyezik meg!"
+            tempErr["RePassword"] = "A két jelszó nem egyezik meg!"
+            setErrors(tempErr)
+            return false
+        }
+
+        if(!new RegExp(".{8}").test(dict.password)){
+            tempErr["Password"] = "A jelszavadnak legalább 8 karakterből kell állnia!"
+            tempErr["RePassword"] = "A jelszavadnak legalább 8 karakterből kell állnia!"
+            setErrors(tempErr)
+            return false
+        }
+        if(!new RegExp("(?=.*[A-Z])").test(dict.password)){
+            tempErr["Password"] = "A jelszavadnak tartalmaznia kell legalább 1 nagy betűt!"
+            tempErr["RePassword"] = "A jelszavadnak tartalmaznia kell legalább 1 nagy betűt!"
+            setErrors(tempErr)
+            return false
+        }
+        if(!new RegExp("(?=.*[0-9])").test(dict.password)){
+            tempErr["Password"] = "A jelszavadnak tartalmaznia kell legalább 1 számot!"
+            tempErr["RePassword"] = "A jelszavadnak tartalmaznia kell legalább 1 számot!"
+            setErrors(tempErr)
+            return false
+        }
+
+
+        window.grecaptcha.ready(_ => {
+            window.grecaptcha
+              .execute("6Lek6BMjAAAAAJJWkTr68AMv6jzYsLk1gi7UVFZm", { action: "changePassword" })
+              .then(token => {
+                fetch("http://127.0.0.1:5000/recaptcha", {
+                    method: "POST",
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({token: token})
+                }).then((response) => response.json()).then((data) => {
+                    if(data.response){
+                        dict["EditKey"] = Key        
+                        fetch('http://127.0.0.1:5000/changePassword', {
+                            method: "POST",
+                            headers: {
+                                'Content-type': 'application/json'
+                            },
+                            body: JSON.stringify(dict)
+                        }).then((response) => response.json()).then((data) => {
+                            setErrors({...data.Errors})
+                            setTimeout(function() {
+                                navigate("/login");
+                              }, 3000);
+                            })
+                    }
+                })
+              })
+        })
+    }
+
     useEffect(() => {
-        if (Display.props.children === "Hiba a szerverre való kapcsolódás során!"){
+        if (Display === "NoCon"){
             fetch("http://127.0.0.1:5000/checkNewPasswordKey", {
                 method: "POST",
                 headers: {
@@ -32,32 +139,30 @@ export default function ForgottenPasswordForm() {
                 },
                 body: JSON.stringify({EditKey: Key})
             }).then((response) => response.json()).then((data) => {
-                console.log(data)
                 if(data.State === true){
-                    setDisplay(        
-                    <React.Fragment>
-                        <form onSubmit={(event) => {
-                            event.preventDefault()
-                            }}>
-                            <div className='forgotExplain'>Add meg az új jelszavadat!</div>
-                            <InputPlus left="15%" width="70%" top="45%" height="5%"  type="password" placeholder="Jelszó" imageSrc="Key" autocompleteID="new-password" dataSet={(txt) => {setPassword(txt); clearError("Password")}} ErrorMsg={Errors["Password"]}/>
-                            <InputPlus left="15%" width="70%" top="60%" height="5%"  type="password" placeholder="Jelszó megerősítés" imageSrc="Key" autocompleteID="new-password" dataSet={(txt) => {setRePassword(txt); clearError("RePassword")}} ErrorMsg={Errors["RePassword"]}/>
-                            <button type="submit" className='loginBtn rounded-pill' style={{
-                                top: "80%"
-                            }}>Megváltoztatás</button>
-                        </form>
-                    </React.Fragment>
-                    )
+                    setDisplay("Page")
                 }else{
-                    setDisplay(<div className='noLink'>{data.State}</div>)
+                    setDisplay(data.State)
                 }
             })
-        }    
+        }
     })
 
-    return (
-        Display
-    );
+    switch(Display){
+        case "NoCon": return (<div className='noLink'>Hiba a szerverre való kapcsolódás során!</div>)
+        case "Page": return getPage()
+        default: return <div className='noLink'>{Display}</div>
+    }
+
+}
+
+function isThereError(list){
+    for (const data of Object.entries(list)) {
+        if(data[1] !== ""){
+            return true
+        }
+    }
+    return false
 }
 
 function isFieldEmpty(text){
