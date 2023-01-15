a01f3132f21bfea12648e6dbca27b54ca220a417
Csóka Csaba <acsokacsaba@gmail.com>
2023-01-15 20:23:37 +0100

    Started MyBusiness subpage

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Outlet } from 'react-router';
 import MenuBar from './MenuBar';
+
+import "./css/Dashboard.css"
 
 function Dasboard() {
     return ( 
         <div className='dashboardContainer'>
-            <MenuBar />
+            <div className='menuDashContainer'>
+                <MenuBar />
+            </div>
+            <div className='contentContainer'>
+                <Outlet />
+            </div>
+                
         </div>
      );
 }
--- src/Components/Dashboard/MyBusinesses.jsx
+++ src/Components/Dashboard/MyBusinesses.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import "./css/MyBusinesses.css"
+
 function MyBusinesses() {
-    return ( <div>haló</div> );
+    return (
+        <React.Fragment>
+            <div className='contentBackground'></div>
+
+            <div className='subOptionsMenu'>               
+                <div className='option'>
+                    <div className='text'>Vállalkozásaim</div>
+                    <div className='line left'></div>
+                </div>
+                <div className='option'>
+                    <div className='text'>Vállalkozás létrehozása</div>
+                    <div className='line'></div>
+                </div>
+                <div className='option'>
+                    <div className='text'>Meghívások</div>
+                    <div className='line right'></div>
+                </div>
+            </div>
+                
+        </React.Fragment>    
+    );
 }
 
 export default MyBusinesses;
