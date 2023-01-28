0c9f9b93fbddd89ab83be5c170fd93818d1a730c
Csóka Csaba <acsokacsaba@gmail.com>
2023-01-28 14:53:11 +0100

    Checking if is userToken exists

--- backend/paths/getAllOrganizations.js
+++ backend/paths/getAllOrganizations.js
@@ -1,9 +1,11 @@
 module.exports = (app, sql_con, jwt) => {
     app.get("/getAllOrganizations", async (req, res) => {
-        var data = jwt.verify(req.cookies.userToken, process.env.JWT_KEY)
-        if(data != null){
-            const getOrgs = await await sql_con.promise().query("SELECT BusinessID, Name, Address, BusinessEmail, BusinessPhone, OwnerName FROM ReserveIt_Businesses")
-            res.send({payload: getOrgs[0]})
+        if(req.cookies.userToken != null){
+            var data = jwt.verify(req.cookies.userToken, process.env.JWT_KEY)
+            if(data != null){
+                const getOrgs = await await sql_con.promise().query("SELECT BusinessID, Name, Address, BusinessEmail, BusinessPhone, OwnerName FROM ReserveIt_Businesses")
+                res.send({payload: getOrgs[0]})
+            }
         }
     })
 }
--- backend/paths/getOrganizations.js
+++ backend/paths/getOrganizations.js
@@ -1,9 +1,11 @@
 module.exports = (app, sql_con, jwt) => {
     app.get("/getOrganizations", async (req, res) => {
-        var data = jwt.verify(req.cookies.userToken, process.env.JWT_KEY)
-        if(data != null){
-            const getOrgs = await await sql_con.promise().query("SELECT ReserveIt_Businesses.BusinessID, Name, Address, BusinessEmail, BusinessPhone, OwnerName FROM ReserveIt_BusinessEmployees INNER JOIN ReserveIt_Businesses ON ReserveIt_BusinessEmployees.BusinessID = ReserveIt_Businesses.BusinessID WHERE AccountID=?", [data.AccountID])
-            res.send({payload: getOrgs[0]})
+        if(req.cookies.userToken != null){
+            var data = jwt.verify(req.cookies.userToken, process.env.JWT_KEY)
+            if(data != null){
+                const getOrgs = await await sql_con.promise().query("SELECT ReserveIt_Businesses.BusinessID, Name, Address, BusinessEmail, BusinessPhone, OwnerName FROM ReserveIt_BusinessEmployees INNER JOIN ReserveIt_Businesses ON ReserveIt_BusinessEmployees.BusinessID = ReserveIt_Businesses.BusinessID WHERE AccountID=?", [data.AccountID])
+                res.send({payload: getOrgs[0]})
+            }
         }
     })
 }
