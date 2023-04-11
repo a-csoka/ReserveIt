7e61ecc8ef51dd3fcc651fb7f236313d2ed37690
Csóka Csaba <acsokacsaba@gmail.com>
2023-04-11 17:42:43 +0200

    Added some status code responses

--- backend/paths/addReservation.js
+++ backend/paths/addReservation.js
@@ -5,58 +5,76 @@ module.exports = (app, sql_con, jwt, isFieldEmpty, moment) => {
             if(data != null){
                 if(req.body.BusinessID !== null && req.body.Name !== null && req.body.WorkerID !== null && req.body.Date !== null && req.body.Start !== null && req.body.End !== null && req.body.Price !== null && req.body.Email !== null){
                     if(isFieldEmpty(req.body.Name)){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     if(isFieldEmpty(req.body.WorkerID)){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     if(isFieldEmpty(req.body.Start)){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     if(isFieldEmpty(req.body.End)){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     if(typeof(parseInt(req.body.Price)) !== "number"){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     if(isFieldEmpty(req.body.Email)){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     if((new Date(req.body.Date+" "+req.body.Start)).getTime() >= (new Date(req.body.Date+" "+req.body.End)).getTime()){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     if(moment(req.body.Date+" "+req.body.Start).isSameOrBefore(new Date())){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     const isWorker = await sql_con.promise().query("SELECT AccountID FROM ReserveIt_BusinessEmployees WHERE BusinessID=? AND AccountID=?", [req.body.BusinessID, data.AccountID])
                     if(isWorker[0].length === 0){
-                        return false
+                        res.status(401).send()
+                        return
                     }
                     const doesExist = await sql_con.promise().query("SELECT AccountID FROM ReserveIt_Accounts WHERE Email=?", [req.body.Email])
                     if(doesExist[0].length === 0){
-                        return false
+                        res.status(400).send()
+                        return
                     }
                     const doesWorkerExist = await sql_con.promise().query("SELECT AccountID FROM ReserveIt_BusinessEmployees WHERE BusinessID=? AND AccountID=?", [req.body.BusinessID, req.body.WorkerID])
                     if(doesWorkerExist[0].length === 0){
-                        return false
+                        res.status(400).send()
+                        return
                     } 
                     const doesOverlap = await sql_con.promise().query("SELECT * FROM ReserveIt_Reservations WHERE ? < End AND ? > Start AND BusinessID=? AND WorkerID=? LIMIT 1", [req.body.Date+" "+req.body.Start, req.body.Date+" "+req.body.End, req.body.BusinessID,req.body.WorkerID])
                     if(doesOverlap[0].length > 0){
-                        res.send({errorMsg: "Ebben az intervallumban már van egy időpontja a dolgozónak!"})
-                        return false
+                        res.status(400).send({errorMsg: "Ebben az intervallumban már van egy időpontja a dolgozónak!"})
+                        return
                     }
                     /*
                     const doesOverlap2 = await sql_con.promise().query("SELECT * FROM ReserveIt_Reservations WHERE ? < End AND ? > Start AND ReserverID=? LIMIT 1", [req.body.Date+" "+req.body.Start, req.body.Date+" "+req.body.End, doesExist[0][0].AccountID])
                     if(doesOverlap2[0].length > 0){
                         res.send({errorMsg: "Ebben az intervallumban már van egy időpontja az ügyfélnek!"})
-                        return false
+                        return
                     }
                     */
                     await sql_con.promise().query("INSERT INTO ReserveIt_Notifications(AccountID, BusinessID, Text) VALUES(?, ?, ?)", [doesExist[0][0].AccountID, req.body.BusinessID, "Hozzá lett adva egy időpont a naptáradhoz! ("+(req.body.Date).replaceAll("-", ". ")+". "+req.body.Start+"-"+req.body.End+")"])
                     const createReservation =  await sql_con.promise().query("INSERT INTO ReserveIt_Reservations(Name, ReserverID, WorkerID, BusinessID, Start, End, Price, Phone, Status) VALUES(?, ?, ?, ?, ?, ?, ?, ?, 'Pending')", [req.body.Name, doesExist[0][0].AccountID, req.body.WorkerID, req.body.BusinessID, req.body.Date+" "+req.body.Start, req.body.Date+" "+req.body.End, req.body.Price, req.body.Phone])
-                    res.send({errorMsg: "Foglalás létrehozva!"})
+                    res.status(201).send({errorMsg: "Foglalás létrehozva!"})
+                    return
                 }
+                res.status(400).send()
+                return
             }
+            res.status(401).send()
+            return
         }
+        res.status(401).send()
+        return
     })
 }
--- backend/paths/checkNewPasswordKey.js
+++ backend/paths/checkNewPasswordKey.js
@@ -1,21 +1,23 @@
 module.exports = (app, sql_con) => {
     app.post("/checkNewPasswordKey", async (req, res) => {
-
         sql_con.query("SELECT * FROM ReserveIt_ForgottenPasswordData WHERE VerificationID=? LIMIT 1", [req.body.EditKey], function (err, result){
             if(result.length == 0){
-                res.send({State: "Nem felismerhető link!"})
-                return false
+                res.status(400).send({State: "Nem felismerhető link!"})
+                return
             }
             
             var recordTime = JSON.parse(JSON.stringify(result[0].Time));
             var nowTime = Date.now();
             var recordTime = new Date(result[0].Time).getTime();
             if (recordTime+600000 < nowTime) {
-                res.send({State: "Ez a link már lejárt!"})
-                return false
+                res.status(401).send({State: "Ez a link már lejárt!"})
+                return
             }
 
-            res.send({State: true})
+            res.status(200).send({State: true})
+            return
         })
+        res.status(500).send()
+        return
     })
 }
