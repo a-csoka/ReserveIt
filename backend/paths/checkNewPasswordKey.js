26bcc7256ef75c0172cec94338b943257da06b38
ACsóka <acsokacsaba@gmail.com>
2023-04-12 14:04:41 +0200

    Updated old sql queries

--- backend/paths/checkNewPasswordKey.js
+++ backend/paths/checkNewPasswordKey.js
@@ -1,23 +1,20 @@
 module.exports = (app, sql_con) => {
     app.post("/checkNewPasswordKey", async (req, res) => {
-        sql_con.query("SELECT * FROM ReserveIt_ForgottenPasswordData WHERE VerificationID=? LIMIT 1", [req.body.EditKey], function (err, result){
-            if(result.length == 0){
-                res.status(400).send({State: "Nem felismerhető link!"})
-                return
-            }
-            
-            var recordTime = JSON.parse(JSON.stringify(result[0].Time));
-            var nowTime = Date.now();
-            var recordTime = new Date(result[0].Time).getTime();
-            if (recordTime+600000 < nowTime) {
-                res.status(401).send({State: "Ez a link már lejárt!"})
-                return
-            }
-
-            res.status(200).send({State: true})
+        const pdata = await sql_con.promise().query("SELECT * FROM ReserveIt_ForgottenPasswordData WHERE VerificationID=? LIMIT 1", [req.body.EditKey])
+        if(pdata[0].length == 0){
+            res.status(400).send({State: "Nem felismerhető link!"})
             return
-        })
-        res.status(500).send()
+        }
+        
+        var recordTime = JSON.parse(JSON.stringify(pdata[0][0].Time));
+        var nowTime = Date.now();
+        var recordTime = new Date(pdata[0][0].Time).getTime();
+        if (recordTime+600000 < nowTime) {
+            res.status(401).send({State: "Ez a link már lejárt!"})
+            return
+        }
+
+        res.status(200).send({State: true})
         return
     })
 }
--- backend/paths/forgottenPassword.js
+++ backend/paths/forgottenPassword.js
@@ -3,53 +3,49 @@ module.exports = (app, sql_con, crypto, mail_con) => {
         var tempErr = {
             "Email": "Amennyiben megfelelő a cím elküldtük a levelet!",
         }
-        sql_con.query("SELECT AccountID,Email FROM ReserveIt_Accounts WHERE email=? LIMIT 1", [req.body.email], function (err, resultOrigin){
-            if (err) throw err;
-            if(resultOrigin.length === 0){
+        const data = await sql_con.promise().query("SELECT AccountID,Email FROM ReserveIt_Accounts WHERE email=? LIMIT 1", [req.body.email])
+        if(data[0].length === 0){
+            res.send(tempErr)
+            return false
+        }
+        const isFlood = await sql_con.promise().query("SELECT Time FROM ReserveIt_ForgottenPasswordData WHERE AccountID=? LIMIT 1", [data[0][0].AccountID])
+        if(isFlood[0].length === 1){
+            var recordTime = JSON.parse(JSON.stringify(isFlood[0].Time));
+            var nowTime = Date.now();
+            var recordTime = new Date(isFlood[0].Time).getTime();
+            if (recordTime+600000 >= nowTime) {
+                tempErr["Email"] = `Csak 10 percenként küldhetsz elfelejtett jelszó kérelmet! Következő lehetőség ${Math.ceil((recordTime+600000-nowTime)/1000/60)} perc múlva!`
                 res.send(tempErr)
                 return false
             }
-            sql_con.query("SELECT Time FROM ReserveIt_ForgottenPasswordData WHERE AccountID=? LIMIT 1", [resultOrigin[0].AccountID], function (err, result){
-                if(result.length === 1){
-                    var recordTime = JSON.parse(JSON.stringify(result[0].Time));
-                    var nowTime = Date.now();
-                    var recordTime = new Date(result[0].Time).getTime();
-                    if (recordTime+600000 >= nowTime) {
-                        tempErr["Email"] = `Csak 10 percenként küldhetsz elfelejtett jelszó kérelmet! Következő lehetőség ${Math.ceil((recordTime+600000-nowTime)/1000/60)} perc múlva!`
-                        res.send(tempErr)
-                        return false
-                    }
+        }
+        await sql_con.promise().query("DELETE FROM ReserveIt_ForgottenPasswordData WHERE AccountID=?", [data[0][0].AccountID])
+
+        await crypto.randomBytes(22, function(err, buffer) {
+            var token = data[0][0].AccountID+buffer.toString('hex');
+
+            mail_con.sendMail({
+                from: "Team ReserveIt <helpdesk.reserveit@gmail.com>",
+                to: data[0][0].Email,
+                subject: "ReserveIt - Elfelejtett jelszó",
+                html: `Tisztelt felhasználó!
+                <br>
+                Erre a címre elfelejtett jelszó kérelem érkezett! A jelszava megváltoztatásához kattintson ide: <b><a href='http://`+req.hostname+`:3000/loginPage/newpassword/`+token+`'>Új jelszó</a></b>
+                <br><br>
+                Amennyiben nem ön volt, kérjük ne tegyen semmit.
+                <br>
+                <br>
+                Tisztelettel,<br>
+                Team ReserveIt
+                `,
+            }, async function(erR, info){
+                if (err) throw err
+                if(!err){
+                    await sql_con.promise().query("INSERT INTO ReserveIt_ForgottenPasswordData(AccountID, VerificationID) VALUES(?,?)", [data[0][0].AccountID, token])
+                    console.log("[Mail]: Elfelejtett jelszó kérelem! [Cím: "+data[0][0].Email+"]")
+                    res.send(tempErr)
                 }
-                sql_con.query("DELETE FROM ReserveIt_ForgottenPasswordData WHERE AccountID=?", [resultOrigin[0].AccountID])
-    
-                crypto.randomBytes(22, function(err, buffer) {
-                    var token = resultOrigin[0].AccountID+buffer.toString('hex');
-    
-                    mail_con.sendMail({
-                        from: "Team ReserveIt <helpdesk.reserveit@gmail.com>",
-                        to: resultOrigin[0].Email,
-                        subject: "ReserveIt - Elfelejtett jelszó",
-                        html: `Tisztelt felhasználó!
-                        <br>
-                        Erre a címre elfelejtett jelszó kérelem érkezett! A jelszava megváltoztatásához kattintson ide: <b><a href='http://`+req.hostname+`:3000/loginPage/newpassword/`+token+`'>Új jelszó</a></b>
-                        <br><br>
-                        Amennyiben nem ön volt, kérjük ne tegyen semmit.
-                        <br>
-                        <br>
-                        Tisztelettel,<br>
-                        Team ReserveIt
-                        `,
-                    }, function(erR, info){
-                        if (err) throw err
-                        if(!err){
-                            sql_con.query("INSERT INTO ReserveIt_ForgottenPasswordData(AccountID, VerificationID) VALUES(?,?)", [resultOrigin[0].AccountID, token])
-                            console.log("[Mail]: Elfelejtett jelszó kérelem! [Cím: "+resultOrigin[0].Email+"]")
-                            res.send(tempErr)
-                        }
-                    });
-                });
-    
-            })
-        })
+            });
+        });
     })
 }
